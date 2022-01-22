d30f97ae1271db6fc0edb19cbedff6667f7477c8
aleksandarLazic1998 <aleksandar.lazic.p@gmail.com>
2022-01-22 19:32:48 +0100

    Add Test For First Request

---
+++ src/Routers/Planets/__tests__/get.test.js
@@ -0,0 +1,15 @@
+const request = require("supertest");
+
+const app = require("../../../app");
+const API_URL = require("../../../Routes");
+
+describe("GET /planets", () => {
+	test("should respond with status 200 and return data", async () => {
+		const response = await request(app)
+			.get(API_URL.PLANETS_URL.BASE_URL)
+			.expect("Content-Type", /json/)
+			.expect(200);
+
+		expect(response.body.length).not.toHaveLength(0);
+	});
+});
